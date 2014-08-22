04e9a341bdfe945b28d8347be4ae9ac521086bc0
Ben TN (AB) <b.torbennielsen@oist.jp>
2014-08-22 14:35:23 +0900

    Add random_walk example

---
+++ examples/run_random_walk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# uncomment next line for interactive checking of generated output
+PYTHON="ipython2 --pylab -i"
+# non-interactive shell. Check results afterwards
+PYTHON="python2.7"
+
+# Random walk
+# Not truly a rondom walk as the generated structure will terminate \
+# when a structural overlap occurs. Detecting and avoiding a \
+# structual ooverlap is a main feature of NeuroMaC
+time PYTHONPATH=random_walk/:$PYTHONPATH python ../Admin.py 1 random_walk/random_walk.cfg 
+python ../scripts/generate_SWC_data.py random_walk/random_walk.cfg random_walk/random_walk.db
+python ../scripts/helper_generate_movie.py random_walk/random_walk.cfg random_walk/random_walk.db
+$PYTHON ../scripts/generate_wire_plot.py random_walk/random_walk.cfg random_walk/random_walk.db
+$PYTHON ../scripts/generate_radii_plot.py random_walk/random_walk.cfg random_walk/random_walk.db
