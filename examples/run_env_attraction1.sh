ad2342cc8859f9e1a1c6714a34c3f13ac11728db
Ben TN (AB) <b.torbennielsen@oist.jp>
2014-08-26 08:01:44 +0900

    Add basic example of interaction with environment. \nHere interaction is attraction of a neurite to the pia; a structure defined in the configuration file

---
+++ examples/run_env_attraction1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# uncomment next line for interactive checking of generated output
+PYTHON="ipython2 --pylab -i"
+# non-interactive shell. Check results afterwards
+PYTHON="python2.7"
+
+# Straight to pia
+time PYTHONPATH=straight_to_pia/:$PYTHONPATH python ../Admin.py 1 straight_to_pia/straight.cfg
+python ../scripts/helper_generate_movie.py straight_to_pia/straight.cfg straight_to_pia/straight.db
+$PYTHON ../scripts/generate_wire_plot.py straight_to_pia/straight.cfg straight_to_pia/straight.db
+$PYTHON ../scripts/generate_radii_plot.py straight_to_pia/straight.cfg straight_to_pia/straight.db
