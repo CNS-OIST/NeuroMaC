4527905fafd204796e45a121c9c4e16d517ce2b2
Ben TN (AB) <b.torbennielsen@oist.jp>
2014-10-03 11:32:30 +0900

    Movie helper script. Remove marker. Introduce basic progress-indicator

--- scripts/helper_generate_movie.py
+++ scripts/helper_generate_movie.py
@@ -30,12 +30,12 @@ if __name__ == "__main__" :
             comment='Movie support!')
     writer = FFMpegWriter(fps=5, metadata=metadata)
         
-    fig = plt.figure(1) # the main figure woth skeleton plot
+    fig = plt.figure(1,figsize=(6,10)) # the main figure woth skeleton plot
     ax = fig.add_subplot(111, projection='3d')
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
-    xmin,ymin,zmin = 0.0, 0.0, 0.0 
+    xmin,ymin,zmin = 0.0, 0.0, 500.0 
     dim_xyz = eval(parser.get("substrate","dim_xyz"))
     xmax = dim_xyz[0]
     ymax = dim_xyz[1]
@@ -44,6 +44,7 @@ if __name__ == "__main__" :
     ax.set_ylim([ymin,ymax])
     ax.set_zlim([zmin,zmax])
     ax.view_init(elev=e_start, azim=a_start)
+    #ax.set_aspect('equal')
     if db_name.startswith(".."):
         movie_name = db_name.split("/")[-1].split(".")[0]+".mp4"
     else: 
@@ -76,12 +77,15 @@ if __name__ == "__main__" :
     cursor.execute("select * from swc_data order by id")
     rets = cursor.fetchall()
     colors= ['r','g','b','c','m','k']
+    counter = 0
     with writer.saving(fig,movie_name,100) :
         for row in rets:
-            #print 'row >',row,'<'
+            counter = counter +1
+            if counter % 20 == 0:
+                print 'counter >',counter,'<'
             swc_id,name,swc_type,x1,y1,z1,x2,y2,z2,radius,proc = row
-            plt.plot([x1,x2],[y1,y2],zs=[z1,z2],color=c_mapping[str(name)],marker=m_mapping[str(proc)])
+            plt.plot([x1,x2],[y1,y2],zs=[z1,z2],color=c_mapping[str(name)])#,marker=m_mapping[str(proc)])
             writer.grab_frame()
-        for ii in np.linspace(a_start,a_start+360,20):
-            ax.view_init(elev=e_start, azim=ii)
-            writer.grab_frame()
+        # for ii in np.linspace(a_start,a_start+360,20):
+        #     ax.view_init(elev=e_start, azim=ii)
+        #     writer.grab_frame()
