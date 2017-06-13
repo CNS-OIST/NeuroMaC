9bb337d1cdccca504d026d0ed348b6adc33e2d61
Erik De Schutter <erik@bobbie-3.local>
2017-06-13 16:00:00 +0900

    Python 3 check

--- scripts/generate_radii_plot.py
+++ scripts/generate_radii_plot.py
@@ -23,6 +23,8 @@
 
 import timeit
 import sys,time
+if sys.version_info[0] < 3:
+    raise Exception("Must be using Python 3")
 import random
 import sqlite3
 import copy
@@ -30,14 +32,14 @@ import numpy
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from ConfigParser import SafeConfigParser # For use with Python 2.7
+import configparser
 
 timer = time.time
 
 def plot_with_radii() :    
     cfg_file = sys.argv[1]
     db_name = sys.argv[2]
-    parser = SafeConfigParser()
+    parser = configparser.ConfigParser()
     parser.read(cfg_file)
     colors= ['r','g','b','c','m','k']
     markers = [".","o","s","^","p","*"]
