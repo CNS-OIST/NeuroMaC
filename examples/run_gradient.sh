b22b025e96a48afb9ccd290207ff5d80cba85ee4
Erik De Schutter <erik@bobbie-3.local>
2017-07-03 12:48:19 +0900

    Buggy

--- examples/run_gradient.sh
+++ examples/run_gradient.sh
@@ -5,6 +5,8 @@ PYTHON="ipython2 --pylab -i"
 # non-interactive shell. Check results afterwards
 PYTHON="python"
 
+# BUGGY!!! Wrong pickle file name, but crashes anyway
+
 # Follow a gradient. Attraction depends on the distance from the attractor.
 # The randome xcursions of the neurite become smaller as the neurite approaches
 # the attractor
